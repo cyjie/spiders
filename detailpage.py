42b4f17425fde113facd9753334174acabda92a5
cyjie <124147396@qq.com>
2020-11-28 17:42:32 +0800

    添加多一层商品id目录

--- detailpage.py
+++ detailpage.py
@@ -23,8 +23,9 @@ def DownloadImage(path, gallery_list):
 
 
 if __name__ == "__main__":
-    target_url = "http://mobile.yangkeduo.com/goods.html?goods_id=166152342057"
-    cookie = 'api_uid=Ckwwal/CAcICYABNn1WeAg==; _nano_fp=XpEon5dYlpUyn5TxlT_iGWeODWgiG6rFd2GRjJdi; ua=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F86.0.4240.198%20Safari%2F537.36; webp=1; JSESSIONID=5D424C22EAE73FFCAF3A8DB48A0952AE; PDDAccessToken=OWRV4UTNS77U3ZIZFYT33MACUK67WJ6ULT5TQ6RTGVSFICKALFLA1124026; pdd_user_id=3346939650624; pdd_user_uin=NW4BZLACTCMGFVLATNO5YRTFGM_GEXDA; pdd_vds=gaLUNKnMtzyKmgbjmzEWPUaUQROSijOJQzOgaUigbMnKGjQziJbzNMIkNzOM'
+    target_id = '2993896368'
+    cookie = 'api_uid=Ckwwal/CAcICYABNn1WeAg==; _nano_fp=XpEon5dYlpUyn5TxlT_iGWeODWgiG6rFd2GRjJdi; ua=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F86.0.4240.198%20Safari%2F537.36; webp=1; JSESSIONID=5D424C22EAE73FFCAF3A8DB48A0952AE; PDDAccessToken=V32TFPM5HHO75GG2LE6RPRFEQDBNMN36VJ73QA7RL3NJ2UXEJUWQ1124026; pdd_user_id=3346939650624; pdd_user_uin=NW4BZLACTCMGFVLATNO5YRTFGM_GEXDA; pdd_vds=gaLfNdnLETNwGmQxmNQnEbtwOnQwLBnBaBabLsLbinteIlnDExbLQfPbIfQf'
+    target_url = "http://mobile.yangkeduo.com/goods.html?goods_id=" + target_id
     headers = {
         'cookie': cookie
     }
@@ -56,14 +57,18 @@ if __name__ == "__main__":
             break
         success = True
 
+        if os.path.exists(target_id):
+            shutil.rmtree(target_id)
+        os.mkdir(target_id)
+
         #下载主图
         top_gallery_list = json_obj['store']['initDataObj']['goods']['topGallery']
-        path = '主图/'
+        path = target_id + '/主图/'
         DownloadImage(path, top_gallery_list)
 
         #下载详情页
         detail_gallery_list = json_obj['store']['initDataObj']['goods']['detailGallery']
-        path = '详情页/'
+        path = target_id + '/详情页/'
         DownloadImage(path, detail_gallery_list)
 
     #Cookie失效，弹个提示
